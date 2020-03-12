9aa0261f28d87aa137b68bdf05980ac739deb5e1
Mike Tika <delakofitika@gmail.com>
2020-03-12 15:12:27 -0700

    second commit

---
+++ try.js
@@ -0,0 +1,16 @@
+switch (prompt("What is the weather like?")){
+	case"rainy":
+	   console.log("Remember to take your umbrella.");
+	   break;
+	case: "sunny":
+		console.log("Dress Lightly. ");
+		break;
+	case: "cloudy":
+		console.log("Go outside. ");
+		break;
+	default:
+		console.log("unknown weather type. ");
+		break;
+
+
+}
